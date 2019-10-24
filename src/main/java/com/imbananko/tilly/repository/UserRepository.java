fe66353d815a12c6ef7ad95108922488fe8ee063
konstantin <k.bespalov@tinkoff.ru>
2019-10-24 13:37:04 +0300

    userCache

--- src/main/java/com/imbananko/tilly/repository/UserRepository.java
+++ src/main/java/com/imbananko/tilly/repository/UserRepository.java
@@ -7,21 +7,29 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.telegram.telegrambots.meta.api.objects.User;
 
+import java.util.WeakHashMap;
+
 @Repository
 public class UserRepository {
+  private final WeakHashMap<Integer, User> userCache;
   private final NamedParameterJdbcTemplate template;
   private final Map<String, String> queries;
 
   public UserRepository(NamedParameterJdbcTemplate template, SqlQueries queries) {
     this.template = template;
     this.queries = queries.getQueries();
+    this.userCache = new WeakHashMap<>();
   }
 
   public void saveIfNotExists(User user) {
-    template.update(queries.getOrElse("insertUserIfNotExists", null),
-      new MapSqlParameterSource("userId", user.getId())
-        .addValue("username", user.getUserName())
-        .addValue("firstName", user.getFirstName())
-        .addValue("lastName", user.getLastName()));
+    userCache.computeIfAbsent(user.getId(), ignore -> {
+      template.update(queries.getOrElse("insertUserIfNotExists", null),
+        new MapSqlParameterSource("userId", user.getId())
+          .addValue("username", user.getUserName())
+          .addValue("firstName", user.getFirstName())
+          .addValue("lastName", user.getLastName()));
+
+      return user;
+    });
   }
 }
