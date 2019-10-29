6350f753a99ed7553bee315842262df66bc2b139
eplastun <yeplastun@gmail.com>
2019-10-30 01:47:37 +0300

    replying to chat with shame

--- src/main/java/com/imbananko/tilly/similarity/MemeMatcher.kt
+++ src/main/java/com/imbananko/tilly/similarity/MemeMatcher.kt
@@ -1,6 +1,5 @@
 package com.imbananko.tilly.similarity
 
-import com.github.kilianB.hashAlgorithms.AverageHash
 import com.github.kilianB.hashAlgorithms.PerceptiveHash
 import com.github.kilianB.matcher.persistent.ConsecutiveMatcher
 import io.vavr.control.Option
@@ -10,16 +9,18 @@ import java.io.File
 
 @Component
 class MemeMatcher {
+    private val normalizedHammingDistance = .02
     private val matcher = ConsecutiveMatcher(true).also {
-        it.addHashingAlgorithm(AverageHash(128), .3)
-        it.addHashingAlgorithm(PerceptiveHash(128), .3)
+        it.addHashingAlgorithm(PerceptiveHash(128), .02, true)
     }
 
     fun addMeme(fileId: String, imageFile: File) = matcher.addImage(fileId, imageFile)
 
     fun checkMemeExists(memeId: String, imageFile: File): Try<Option<String?>> = Try.of {
-        Option.of(matcher.getMatchingImages(imageFile).poll()?.value).also {
-            if (it.isEmpty) matcher.addImage(memeId, imageFile)
-        }
+        Option.of(
+                matcher.getMatchingImages(imageFile).poll()
+                        ?.takeIf { it.normalizedHammingDistance < this.normalizedHammingDistance }
+                        ?.value
+        ).also { if (it.isEmpty) matcher.addImage(memeId, imageFile) }
     }
 }
