04d92705eef2a25a2ace1f2003c2c81fd453abd9
imbananko <imbananko@gmail.com>
2020-04-05 20:54:22 +0300

    save vote source to database

--- src/main/kotlin/com/chsdngm/tilly/repository/VoteRepository.kt
+++ src/main/kotlin/com/chsdngm/tilly/repository/VoteRepository.kt
@@ -1,9 +1,6 @@
 package com.chsdngm.tilly.repository
 
-import com.chsdngm.tilly.model.MemeEntity
-import com.chsdngm.tilly.model.MemeStatsEntry
-import com.chsdngm.tilly.model.VoteEntity
-import com.chsdngm.tilly.model.VoteValue.*
+import com.chsdngm.tilly.model.*
 import com.chsdngm.tilly.utility.SqlQueries
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
@@ -11,17 +8,25 @@ import org.springframework.stereotype.Repository
 
 @Repository
 class VoteRepository(private val template: NamedParameterJdbcTemplate, private val queries: SqlQueries) {
-  fun insertOrUpdate(vote: VoteEntity): Int = template.update(queries.getFromConfOrFail("insertOrUpdateVote"), getParams(vote))
+  fun insertOrUpdate(vote: VoteEntity): Int =
+      template.update(queries.getFromConfOrFail("insertOrUpdateVote"),
+          MapSqlParameterSource("chatMessageId", vote.messageId)
+              .addValue("voterId", vote.voterId)
+              .addValue("value", vote.voteValue.name)
+              .addValue("source", vote.source.name))
 
-  fun delete(vote: VoteEntity) = template.update(queries.getFromConfOrFail("deleteVote"), getParams(vote))
+  fun delete(vote: VoteEntity) = template.update(queries.getFromConfOrFail("deleteVote"),
+      MapSqlParameterSource("chatMessageId", vote.messageId)
+          .addValue("voterId", vote.voterId)
+          .addValue("value", vote.voteValue.name))
 
   fun getStatsByUser(userId: Int): List<MemeStatsEntry> = template.query(
       queries.getFromConfOrFail("findUserStats"),
       MapSqlParameterSource("userId", userId))
   { rs, _ ->
     MemeStatsEntry(
-        rs.getInt(UP.name),
-        rs.getInt(DOWN.name),
+        rs.getInt(VoteValue.UP.name),
+        rs.getInt(VoteValue.DOWN.name),
         rs.getBoolean("is_published"))
   }.toList()
 
@@ -32,11 +37,8 @@ class VoteRepository(private val template: NamedParameterJdbcTemplate, private v
     VoteEntity(
         rs.getInt("chat_message_id"),
         rs.getInt("voter_id"),
-        valueOf(rs.getString("value")))
+        VoteValue.valueOf(rs.getString("value")),
+        SourceType.valueOf(rs.getString("source")))
   }.toList()
 
-  private fun getParams(vote: VoteEntity): MapSqlParameterSource =
-      MapSqlParameterSource("chatMessageId", vote.messageId)
-          .addValue("voterId", vote.voterId)
-          .addValue("value", vote.voteValue.name)
 }
