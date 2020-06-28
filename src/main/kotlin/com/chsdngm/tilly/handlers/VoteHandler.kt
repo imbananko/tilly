ace33d809766ea1333436f3350b632b86fc09f13
imbananko <imbananko@gmail.com>
2020-06-28 10:02:26 +0300

    Revert "done migrating"

--- src/main/kotlin/com/chsdngm/tilly/handlers/VoteHandler.kt
+++ src/main/kotlin/com/chsdngm/tilly/handlers/VoteHandler.kt
@@ -2,115 +2,108 @@ package com.chsdngm.tilly.handlers
 
 import com.chsdngm.tilly.model.*
 import com.chsdngm.tilly.repository.MemeRepository
-import com.chsdngm.tilly.utility.TelegramConfig.Companion.CHANNEL_ID
-import com.chsdngm.tilly.utility.TelegramConfig.Companion.CHAT_ID
-import com.chsdngm.tilly.utility.TelegramConfig.Companion.api
-import com.chsdngm.tilly.utility.createMarkup
+import com.chsdngm.tilly.repository.VoteRepository
+import com.chsdngm.tilly.utility.BotConfig.Companion.CHANNEL_ID
+import com.chsdngm.tilly.utility.BotConfig.Companion.CHAT_ID
+import com.chsdngm.tilly.utility.BotConfig.Companion.api
 import com.chsdngm.tilly.utility.hasLocalTag
 import org.slf4j.LoggerFactory
-import org.springframework.stereotype.Service
+import org.springframework.stereotype.Component
 import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText
 
-@Service
-class VoteHandler(private val memeRepository: MemeRepository) : AbstractHandler<VoteUpdate> {
+@Component
+class VoteHandler(private val memeRepository: MemeRepository,
+                  private val voteRepository: VoteRepository) : AbstractHandler<VoteUpdate>() {
 
   private val log = LoggerFactory.getLogger(javaClass)
 
   override fun handle(update: VoteUpdate) {
     val meme = when (update.isFrom) {
-      VoteSourceType.CHANNEL -> memeRepository.findByChannelMessageId(update.messageId)
-      VoteSourceType.CHAT -> memeRepository.findByChatMessageId(update.messageId)
+      SourceType.CHANNEL -> memeRepository.findByChannelMessageId(update.messageId)
+      SourceType.CHAT -> memeRepository.findByChatMessageId(update.messageId)
     } ?: return
 
-    val vote = Vote(VoteKey(meme.chatMessageId, update.fromId), update.voteValue, update.isFrom)
+    val vote = VoteEntity(meme.chatMessageId, update.fromId, update.voteValue, update.isFrom)
 
-    if (update.isNotProcessable || meme.senderId == vote.key.voterId) return
+    if (update.isNotProcessable || meme.senderId == vote.voterId) return
 
-    meme.votes.firstOrNull { it.key.voterId == vote.key.voterId }.also { found ->
-      if (found == null)
-        meme.votes.add(vote)
-      else {
-        meme.votes.remove(found)
-        if (found == vote) {
-          sendPopupNotification(update.callbackQueryId, "Вы удалили свой голос с этого мема")
-        } else {
-          meme.votes.add(found.copy(value = vote.value).apply {
-            when (this.value) {
-              VoteValue.UP -> "Вы обогатили этот мем ${VoteValue.UP.emoji}"
-              VoteValue.DOWN -> "Вы засрали этот мем ${VoteValue.DOWN.emoji}"
-            }.let { sendPopupNotification(update.callbackQueryId, it) }
-          })
+    val votes = voteRepository.getVotes(meme)
+        .associate { Pair(it.voterId, it.voteValue) }
+        .toMutableMap().also {
+          it.merge(vote.voterId, vote.voteValue) { old, new -> if (old == new) null else new }
         }
-      }
+
+    val groupedVotes = votes.values.groupingBy { it }.eachCount()
+    updateChatMarkup(meme.chatMessageId, groupedVotes)
+
+    meme.channelMessageId?.also { updateChannelMarkup(it, groupedVotes) } ?: if (readyForShipment(meme, votes)) {
+      val channelMessageId = sendMemeToChannel(meme, groupedVotes).messageId
+      memeRepository.update(meme, meme.copy(channelMessageId = channelMessageId))
     }
 
-    updateChatMarkup(meme)
-
-    meme.channelMessageId?.let {
-      updateChannelMarkup(meme)
-    } ?: if (readyForShipment(meme))
-      memeRepository.save(meme.copy(channelMessageId = sendMemeToChannel(meme).messageId))
-    else
-      memeRepository.save(meme)
+    if (votes.containsKey(vote.voterId)) {
+      when (vote.voteValue) {
+        VoteValue.UP -> sendPopupNotification(update.callbackQueryId, "Вы обогатили этот мем ${update.voteValue.emoji}")
+        VoteValue.DOWN -> sendPopupNotification(update.callbackQueryId, "Вы засрали этот мем ${update.voteValue.emoji}")
+      }
+      voteRepository.insertOrUpdate(vote)
+    } else {
+      sendPopupNotification(update.callbackQueryId, "Вы удалили свой голос с этого мема")
+      voteRepository.delete(vote)
+    }
 
     runCatching {
-      with(StringBuilder()) {
-        this.append(
-            if (hasLocalTag(meme.caption))
-              "так как мем локальный, на канал он отправлен не будет"
-            else if (meme.channelMessageId != null || readyForShipment(meme))
-              "мем отправлен на канал. "
-            else
-              "мем на модерации. ")
+      val privateCaptionPrefix =
+          if (hasLocalTag(meme.caption)) "так как мем локальный, на канал он отправлен не будет"
+          else if (meme.isPublishedOnChannel() || readyForShipment(meme, votes)) "мем отправлен на канал"
+          else "мем на модерации"
 
-        this.append("статистика: \n\n")
-        this.append(meme.votes.groupingBy { it.value }.eachCount().entries.sortedBy { it.key }.joinToString(
-            transform = { (value, sum) -> "${value.emoji}: $sum" }))
+      val privateChatCaption = groupedVotes.entries.sortedBy { it.key }.joinToString(
+          prefix = "$privateCaptionPrefix. статистика: \n\n",
+          transform = { (value, sum) -> "${value.emoji}: $sum" })
 
-        updateStatsInSenderChat(meme, this.toString())
-      }
+      updateStatsInSenderChat(meme, privateChatCaption)
     }.onFailure { log.error("Failed to update private caption", it) }
 
     log.info("Processed vote update=$update")
   }
 
-  fun sendPopupNotification(callbackQueryId: String, text: String) {
+  private fun sendPopupNotification(callbackQueryId: String, text: String) {
     AnswerCallbackQuery()
         .setCallbackQueryId(callbackQueryId)
         .setText(text).let { api.execute(it) }
   }
 
-  private fun updateChatMarkup(meme: Meme) =
+  private fun updateChatMarkup(messageId: Int, votes: Map<VoteValue, Int>) =
       EditMessageReplyMarkup()
           .setChatId(CHAT_ID)
-          .setMessageId(meme.chatMessageId)
-          .setReplyMarkup(createMarkup(meme.votes.groupingBy { it.value }.eachCount()))
-          .let { api.execute(it) }
+          .setMessageId(messageId)
+          .setReplyMarkup(createMarkup(votes)).let { api.execute(it) }
 
-  private fun updateChannelMarkup(meme: Meme) =
+  private fun updateChannelMarkup(messageId: Int, votes: Map<VoteValue, Int>) =
       EditMessageReplyMarkup()
           .setChatId(CHANNEL_ID)
-          .setMessageId(meme.channelMessageId)
-          .setReplyMarkup(createMarkup(meme.votes.groupingBy { it.value }.eachCount()))
-          .let { api.execute(it) }
+          .setMessageId(messageId)
+          .setReplyMarkup(createMarkup(votes)).let { api.execute(it) }
 
-  private fun sendMemeToChannel(meme: Meme) =
+  private fun sendMemeToChannel(meme: MemeEntity, votes: Map<VoteValue, Int>) =
       SendPhoto()
           .setChatId(CHANNEL_ID)
           .setPhoto(meme.fileId)
-          .setReplyMarkup(createMarkup(meme.votes.groupingBy { it.value }.eachCount()))
+          .setReplyMarkup(createMarkup(votes))
           .setCaption(meme.caption)
           .let { api.execute(it) }
           .also { log.info("Sent meme to channel=$meme") }
 
-  private fun readyForShipment(meme: Meme): Boolean = with(meme.votes.map { it.value }) {
-    this.filter { it == VoteValue.UP }.size - this.filter { it == VoteValue.DOWN }.size >= 5 && !hasLocalTag(meme.caption)
-  }
+  private fun readyForShipment(meme: MemeEntity, votes: MutableMap<Int, VoteValue>): Boolean =
+      (votes.values.filter { it == VoteValue.UP }.size - votes.values.filter { it == VoteValue.DOWN }.size) >= 5 &&
+          !hasLocalTag(meme.caption)
 
-  private fun updateStatsInSenderChat(meme: Meme, stats: String) =
+
+  private fun updateStatsInSenderChat(meme: MemeEntity, stats: String) =
       EditMessageText()
           .setChatId(meme.senderId.toString())
           .setMessageId(meme.privateMessageId)
--- src/main/kotlin/com/chsdngm/tilly/repository/MemeRepository.kt
+++ src/main/kotlin/com/chsdngm/tilly/repository/MemeRepository.kt
@@ -1,40 +1,113 @@
 package com.chsdngm.tilly.repository
 
-import com.chsdngm.tilly.model.Meme
-import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.CrudRepository
-import org.springframework.data.repository.query.Param
+import com.chsdngm.tilly.model.MemeEntity
+import com.chsdngm.tilly.utility.SqlQueries
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Repository
+import java.sql.ResultSet
+import java.util.*
 
 @Repository
-interface MemeRepository : CrudRepository<Meme, Long> {
-  fun findByChatMessageId(messageId: Int): Meme?
+class MemeRepository(private val template: NamedParameterJdbcTemplate, private val queries: SqlQueries) {
+  private val memeSenderCache: WeakHashMap<Int, Int> = WeakHashMap()
 
-  fun findByChannelMessageId(messageId: Int): Meme?
+  fun save(meme: MemeEntity): MemeEntity {
+    template.update(queries.getFromConfOrFail("insertMeme"),
+        MapSqlParameterSource("chatMessageId", meme.chatMessageId)
+            .addValue("senderId", meme.senderId)
+            .addValue("fileId", meme.fileId)
+            .addValue("privateMessageId", meme.privateMessageId)
+            .addValue("caption", meme.caption))
 
-  fun findByFileId(fileId: String): Meme?
+    memeSenderCache[meme.chatMessageId] = meme.senderId
+    return meme
+  }
 
-  @Query("""
-        select distinct meme 
-        from Meme meme 
-        left join fetch meme.votes
-        where meme.senderId = ?1
-        """)
-  fun findBySenderId(senderId: Int): List<Meme>
+  fun findByChatMessageId(chatMessageId: Int): MemeEntity? =
+      template.queryForObject(queries.getFromConfOrFail("findMemeByMessageId"),
+          MapSqlParameterSource("chatMessageId", chatMessageId))
+      { rs, _ ->
+        MemeEntity(rs.getInt("chat_message_id"),
+            rs.getInt("sender_id"),
+            rs.getString("file_id"),
+            rs.getString("caption"),
+            checkZero(rs.getInt("private_message_id")),
+            checkZero(rs.getInt("channel_message_id")))
+      }
 
-  @Query(value = "insert into meme_of_week (chat_message_id) values (:chatMessageId)", nativeQuery = true)
-  fun saveMemeOfWeek(@Param("chatMessageId") chatMessageId: Int)
+  fun findByChannelMessageId(channelMessageId: Int): MemeEntity? =
+      template.queryForObject(queries.getFromConfOrFail("findMemeByChannelMessageId"),
+          MapSqlParameterSource("channelMessageId", channelMessageId))
+      { rs, _ ->
+        MemeEntity(rs.getInt("chat_message_id"),
+            rs.getInt("sender_id"),
+            rs.getString("file_id"),
+            rs.getString("caption"),
+            checkZero(rs.getInt("private_message_id")),
+            checkZero(rs.getInt("channel_message_id")))
+      }
 
-  @Query(value = """
-    select m.*
-    from meme m
-             join vote v on m.chat_message_id = v.chat_message_id
-    where m.created_at > current_timestamp - interval '7 days'
-    group by m.chat_message_id
-    order by count(value) filter (where value = 'UP') - count(value) filter (where value = 'DOWN') desc
-    limit 1
-  """, nativeQuery = true)
-  fun findMemeOfTheWeek(): Meme?
+  fun findByFileId(filedId: String): MemeEntity? =
+      template.queryForObject(queries.getFromConfOrFail("findMemeByFileId"),
+          MapSqlParameterSource("fileId", filedId))
+      { rs, _ ->
+        MemeEntity(rs.getInt("chat_message_id"),
+            rs.getInt("sender_id"),
+            rs.getString("file_id"),
+            rs.getString("caption"),
+            checkZero(rs.getInt("private_message_id")),
+            checkZero(rs.getInt("channel_message_id")))
+      }
+
+  fun findAll(): List<MemeEntity> =
+      template.query(queries.getFromConfOrFail("selectAllMemes"))
+      { rs: ResultSet, _: Int ->
+        MemeEntity(rs.getInt("chat_message_id"),
+            rs.getInt("sender_id"),
+            rs.getString("file_id"),
+            rs.getString("caption"),
+            checkZero(rs.getInt("private_message_id")),
+            checkZero(rs.getInt("channel_message_id")))
+      }
+
+  fun findMemeOfTheWeek(): MemeEntity? =
+      template.queryForObject(queries.getFromConfOrFail("getMemeOfTheWeek"), MapSqlParameterSource())
+      { rs: ResultSet, _: Int ->
+        MemeEntity(rs.getInt("chat_message_id"),
+            rs.getInt("sender_id"),
+            rs.getString("file_id"),
+            rs.getString("caption"),
+            checkZero(rs.getInt("private_message_id")),
+            checkZero(rs.getInt("channel_message_id")))
+      }
+
+  fun findMemesOfTheYear(): List<MemeEntity> =
+      template.query(queries.getFromConfOrFail("getMemesOfTheYear"))
+      { rs: ResultSet, _: Int ->
+        MemeEntity(rs.getInt("chat_message_id"),
+            rs.getInt("sender_id"),
+            rs.getString("file_id"),
+            rs.getString("caption"),
+            checkZero(rs.getInt("private_message_id")),
+            checkZero(rs.getInt("channel_message_id")))
+      }.toList()
+
+  fun markAsMemeOfTheWeek(meme: MemeEntity) =
+      template.update(queries.getFromConfOrFail("insertMemeOfWeek"),
+          MapSqlParameterSource("chatMessageId", meme.chatMessageId))
+
+  fun update(old: MemeEntity, new: MemeEntity) =
+      template.update(queries.getFromConfOrFail("updateMeme"),
+          MapSqlParameterSource("oldMessageId", old.chatMessageId)
+              .addValue("newMessageId", new.chatMessageId)
+              .addValue("oldSenderId", old.senderId)
+              .addValue("newSenderId", new.senderId)
+              .addValue("oldFileId", old.fileId)
+              .addValue("newFileId", new.fileId)
+              .addValue("oldChannelMessageId", old.channelMessageId)
+              .addValue("newChannelMessageId", new.channelMessageId)
+      )
+
+  private fun <T> checkZero(param: T): T? = if (param == 0 || param == 0L) null else param
 }
-
-
