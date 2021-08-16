285f3eff92f04e3594bb4c4f5b69819cbc9ea315
konstantin <k.bespalov@tinkoff.ru>
2021-08-16 08:24:01 +0300

    remove ebuchka image

--- src/main/kotlin/com/chsdngm/tilly/Schedulers.kt
+++ src/main/kotlin/com/chsdngm/tilly/Schedulers.kt
@@ -5,12 +5,9 @@ import com.chsdngm.tilly.repository.MemeRepository
 import com.chsdngm.tilly.utility.TillyConfig
 import com.chsdngm.tilly.utility.TillyConfig.Companion.BETA_CHAT_ID
 import com.chsdngm.tilly.utility.TillyConfig.Companion.CHANNEL_ID
-import com.chsdngm.tilly.utility.TillyConfig.Companion.CHAT_ID
 import com.chsdngm.tilly.utility.TillyConfig.Companion.api
 import com.chsdngm.tilly.utility.mention
-import org.apache.commons.io.FileUtils
 import org.slf4j.LoggerFactory
-import org.springframework.core.io.ClassPathResource
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
@@ -18,9 +15,6 @@ import org.telegram.telegrambots.meta.api.methods.ParseMode
 import org.telegram.telegrambots.meta.api.methods.groupadministration.GetChatMember
 import org.telegram.telegrambots.meta.api.methods.pinnedmessages.PinChatMessage
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
-import org.telegram.telegrambots.meta.api.methods.send.SendPhoto
-import org.telegram.telegrambots.meta.api.objects.InputFile
-import java.io.File
 
 @Service
 @EnableScheduling
@@ -30,12 +24,6 @@ final class Schedulers(
 ) {
 
   private val log = LoggerFactory.getLogger(javaClass)
-
-  private val ebuchkaImage = File("ebuchka").also {
-    ClassPathResource("images/ebuchka.jpg").apply {
-      FileUtils.copyInputStreamToFile(this.inputStream, it)
-    }
-  }
 
   // every hour since 8 am till 1 am Moscow time
   @Scheduled(cron = "0 0 5-22/1 * * *")
@@ -86,19 +74,6 @@ final class Schedulers(
           parseMode = ParseMode.HTML
         }.let { method -> api.execute(method) }
       }
-
-  @Scheduled(cron = "0 0 17 * * *")
-  private fun replyOnForgottenMemes() = memeRepository.findForgottenMemes().also { memes ->
-    log.info("Found ${memes.size} forgotten memes. Replying...")
-
-    memes.forEach { meme ->
-      SendPhoto().apply {
-        chatId = CHAT_ID
-        photo = InputFile(ebuchkaImage)
-        replyToMessageId = meme.moderationChatMessageId
-      }.let { api.execute(it) }
-    }
-  }
 }
 
 
