1e79036c97a90902831115eb1132aa44a3b96b42
vkoshkin <vkoshkin@ozon.ru>
2021-12-23 01:57:28 +0300

    ci wip

--- src/main/kotlin/com/chsdngm/tilly/TillyApplication.kt
+++ src/main/kotlin/com/chsdngm/tilly/TillyApplication.kt
@@ -1,11 +1,19 @@
 package com.chsdngm.tilly
 
+import com.chsdngm.tilly.utility.TillyConfig
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.telegram.telegrambots.meta.api.methods.ParseMode
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 
 @SpringBootApplication
 class TillyApplication
 
 fun main(args: Array<String>) {
   SpringApplication.run(TillyApplication::class.java, *args)
+  SendMessage().apply {
+    chatId = TillyConfig.BETA_CHAT_ID
+    text = "${TillyConfig.BOT_USERNAME} started"
+    parseMode = ParseMode.HTML
+  }.let { method -> TillyConfig.api.execute(method) }
 }
