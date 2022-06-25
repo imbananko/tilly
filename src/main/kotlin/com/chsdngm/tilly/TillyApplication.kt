2702cccf227096a5de8c6aa73d329f337618e0e3
imbananko <imbananko@gmail.com>
2022-06-25 03:14:37 +0300

    update gradle

--- src/main/kotlin/com/chsdngm/tilly/TillyApplication.kt
+++ src/main/kotlin/com/chsdngm/tilly/TillyApplication.kt
@@ -2,6 +2,7 @@ package com.chsdngm.tilly
 
 import com.chsdngm.tilly.utility.TillyConfig
 import org.elasticsearch.client.RestHighLevelClient
+import org.jetbrains.exposed.sql.Database
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
@@ -11,6 +12,7 @@ import org.springframework.data.elasticsearch.client.RestClients
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration
 import org.telegram.telegrambots.meta.api.methods.ParseMode
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import javax.sql.DataSource
 
 @SpringBootApplication
 class TillyApplication
@@ -35,5 +37,13 @@ class ElasticsearchClientConfig(val tillyConfig: TillyConfig) : AbstractElastics
             .build()
 
         return RestClients.create(clientConfiguration).rest()
+    }
+}
+
+@Configuration
+class ExposedConfiguration {
+    @Bean
+    fun database(dataSource: DataSource): Database {
+        return Database.connect(dataSource)
     }
 }
