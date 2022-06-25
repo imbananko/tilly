fb937c4b5c67da95627919a3f429dbb0dcbc567d
imbananko <imbananko@gmail.com>
2022-06-25 03:40:20 +0300

    Revert "update gradle"
    This reverts commit 2702cccf227096a5de8c6aa73d329f337618e0e3.

--- src/main/kotlin/com/chsdngm/tilly/TillyApplication.kt
+++ src/main/kotlin/com/chsdngm/tilly/TillyApplication.kt
@@ -2,7 +2,6 @@ package com.chsdngm.tilly
 
 import com.chsdngm.tilly.utility.TillyConfig
 import org.elasticsearch.client.RestHighLevelClient
-import org.jetbrains.exposed.sql.Database
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
@@ -12,7 +11,6 @@ import org.springframework.data.elasticsearch.client.RestClients
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration
 import org.telegram.telegrambots.meta.api.methods.ParseMode
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
-import javax.sql.DataSource
 
 @SpringBootApplication
 class TillyApplication
@@ -37,13 +35,5 @@ class ElasticsearchClientConfig(val tillyConfig: TillyConfig) : AbstractElastics
             .build()
 
         return RestClients.create(clientConfiguration).rest()
-    }
-}
-
-@Configuration
-class ExposedConfiguration {
-    @Bean
-    fun database(dataSource: DataSource): Database {
-        return Database.connect(dataSource)
     }
 }
