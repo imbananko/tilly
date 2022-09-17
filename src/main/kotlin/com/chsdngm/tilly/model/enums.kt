b244310c07dbf652b047c3f4d86d0de7776475bc
imbananko <imbananko@gmail.com>
2022-09-17 18:36:54 +0200

    [resurrection] first iteration

--- src/main/kotlin/com/chsdngm/tilly/model/enums.kt
+++ src/main/kotlin/com/chsdngm/tilly/model/enums.kt
@@ -20,7 +20,8 @@ enum class MemeStatus(val description: String) {
     LOCAL("так как мем локальный, на канал он отправлен не будет."),
     SCHEDULED("мем будет отправлен на канал."),
     PUBLISHED("мем отправлен на канал."),
-    DECLINED("мем предан забвению.")
+    DECLINED("мем предан забвению."),
+    RESURRECTION_ASKED("для мема запрошено воскрешение.")
 }
 
 enum class WeightedModerationType(val weight: Int) {
