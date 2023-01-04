5db83e9b8444498f840efa6567b7c6f201ce01d3
imbananko <imbananko@gmail.com>
2023-01-04 09:23:59 +0100

    add try-catch

--- src/main/kotlin/com/chsdngm/tilly/handlers/ChannelMarkupUpdater.kt
+++ src/main/kotlin/com/chsdngm/tilly/handlers/ChannelMarkupUpdater.kt
@@ -7,6 +7,7 @@ import com.chsdngm.tilly.utility.createMarkup
 import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.subjects.PublishSubject
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup
 import java.util.concurrent.TimeUnit
@@ -14,6 +15,7 @@ import java.util.concurrent.TimeUnit
 @Service
 class ChannelMarkupUpdater {
     private val markupSubject = PublishSubject.create<Pair<Meme, List<Vote>>>()
+    private val log = LoggerFactory.getLogger(javaClass)
 
     init {
         val timeoutObservable = Flowable.interval(/* initialDelay = */ 0, /* period = */ 5, TimeUnit.SECONDS)
@@ -35,10 +37,15 @@ class ChannelMarkupUpdater {
     }
 
     private final fun updateChannelMarkup(meme: Meme, votes: List<Vote>) {
-        EditMessageReplyMarkup().apply {
-            chatId = TelegramConfig.CHANNEL_ID
-            messageId = meme.channelMessageId
-            replyMarkup = createMarkup(votes)
-        }.let { TelegramConfig.api.execute(it) }
+        try {
+            EditMessageReplyMarkup().apply {
+                chatId = TelegramConfig.CHANNEL_ID
+                messageId = meme.channelMessageId
+                replyMarkup = createMarkup(votes)
+            }.let { TelegramConfig.api.execute(it) }
+        } catch (e: Exception) {
+            log.error("Failed to update markup. e=", e)
+        }
+
     }
 }
