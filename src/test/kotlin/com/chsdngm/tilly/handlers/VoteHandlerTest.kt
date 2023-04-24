99a6d15a2620267f22410ee1122197968e6a48ab
imbananko <imbananko@gmail.com>
2023-04-25 01:13:47 +0200

    Remove rateLimiter, add tests

--- src/test/kotlin/com/chsdngm/tilly/handlers/VoteHandlerTest.kt
+++ src/test/kotlin/com/chsdngm/tilly/handlers/VoteHandlerTest.kt
@@ -10,6 +10,7 @@ import com.chsdngm.tilly.model.dto.Vote
 import com.chsdngm.tilly.repository.MemeDao
 import com.chsdngm.tilly.repository.VoteDao
 import com.chsdngm.tilly.schedulers.ChannelMarkupUpdater
+import com.chsdngm.tilly.utility.createMarkup
 import javassist.NotFoundException
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -20,6 +21,8 @@ import org.mockito.kotlin.stub
 import org.mockito.kotlin.verifyBlocking
 import org.mockito.kotlin.whenever
 import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup
+import java.io.Serializable
 import java.time.Instant
 
 class VoteHandlerTest {
@@ -30,7 +33,7 @@ class VoteHandlerTest {
     private val api = mock<TelegramApi>()
     private val telegramProperties = TelegramProperties(
         "montornChatId",
-        "targetChatId",
+        "666",
         "2105",
         "botToken",
         "botUsername",
@@ -122,6 +125,7 @@ class VoteHandlerTest {
         }
 
         whenever(memeDao.findMemeByChannelMessageId(555)).thenReturn(meme to listOf())
+
         val answerCallbackQuery = AnswerCallbackQuery().apply {
             callbackQueryId = "random_callback"
             text = "Вы обогатили этот мем \uD83D\uDC8E"
@@ -130,6 +134,7 @@ class VoteHandlerTest {
         api.stub {
             onBlocking { executeSuspended(answerCallbackQuery) }.thenReturn(true)
         }
+
         voteHandler.handleSync(update)
 
         val vote = Vote(
@@ -150,7 +155,174 @@ class VoteHandlerTest {
 
     @Test
     fun shouldProcessVoteFromGroup() {
-        //TODO
+        val update = mock<VoteUpdate> {
+            on(it.isOld).thenReturn(false)
+            on(it.sourceChatId).thenReturn("666")
+            on(it.voterId).thenReturn(2103)
+            on(it.messageId).thenReturn(555)
+            on(it.callbackQueryId).thenReturn("random_callback")
+            on(it.voteValue).thenReturn(VoteValue.DOWN)
+            on(it.createdAt).thenReturn(1234567)
+        }
+
+        val meme = mock<Meme> {
+            whenever(it.channelMessageId).thenReturn(null)
+            whenever(it.id).thenReturn(1)
+        }
+
+        whenever(memeDao.findMemeByModerationChatIdAndModerationChatMessageId(666, 555))
+            .thenReturn(meme to listOf())
+
+        val answerCallbackQuery = AnswerCallbackQuery().apply {
+            callbackQueryId = "random_callback"
+            text = "Вы засрали этот мем \uD83D\uDCA9"
+        }
+
+        val vote = Vote(
+            1,
+            2103,
+            666,
+            VoteValue.DOWN,
+            Instant.ofEpochMilli(1234567)
+        )
+
+        val editMessageReplyMarkup = EditMessageReplyMarkup().apply {
+            chatId = telegramProperties.targetChatId
+            messageId = meme.moderationChatMessageId
+            replyMarkup = createMarkup(listOf(vote))
+        }
+
+        api.stub {
+            onBlocking { executeSuspended(answerCallbackQuery) }.thenReturn(true)
+            onBlocking { executeSuspended(editMessageReplyMarkup) }.thenReturn(true)
+        }
+
+        voteHandler.handleSync(update)
+
+        verify(memeDao).findMemeByModerationChatIdAndModerationChatMessageId(666, 555)
+        verifyBlocking(voteDao) { insert(vote) }
+        verifyBlocking(api) { api.executeSuspended(answerCallbackQuery) }
+        verifyBlocking(api) { api.executeSuspended(editMessageReplyMarkup) }
+        verifyBlocking(api) { api.updateStatsInSenderChat(meme, listOf(vote)) }
+        verifyNoMoreInteractions(memeDao, voteDao, metricsUtils, channelMarkupUpdater, api)
+    }
+
+    @Test
+    fun shouldRemoveVoteWhenTheSameVoterAndValue() {
+        val update = mock<VoteUpdate> {
+            on(it.isOld).thenReturn(false)
+            on(it.sourceChatId).thenReturn("666")
+            on(it.voterId).thenReturn(2103)
+            on(it.messageId).thenReturn(555)
+            on(it.callbackQueryId).thenReturn("random_callback")
+            on(it.voteValue).thenReturn(VoteValue.DOWN)
+            on(it.createdAt).thenReturn(1234567)
+        }
+
+        val meme = mock<Meme> {
+            whenever(it.channelMessageId).thenReturn(null)
+            whenever(it.moderationChatMessageId).thenReturn(321)
+            whenever(it.id).thenReturn(1)
+        }
+
+        val vote = Vote(
+            1,
+            2103,
+            666,
+            VoteValue.DOWN,
+            Instant.ofEpochMilli(1234567)
+        )
+
+        whenever(memeDao.findMemeByModerationChatIdAndModerationChatMessageId(666, 555))
+            .thenReturn(meme to listOf(vote))
+
+        val answerCallbackQuery = AnswerCallbackQuery().apply {
+            callbackQueryId = "random_callback"
+            text = "Вы удалили свой голос с этого мема"
+        }
+
+        val editMessageReplyMarkup = EditMessageReplyMarkup().apply {
+            chatId = telegramProperties.targetChatId
+            messageId = 321
+            replyMarkup = createMarkup(listOf())
+        }
+
+        api.stub {
+            onBlocking { executeSuspended(answerCallbackQuery) }.thenReturn(true)
+            onBlocking { executeSuspended(editMessageReplyMarkup) }.thenReturn(mock<Serializable>())
+        }
+
+        voteHandler.handleSync(update)
+
+        verify(memeDao).findMemeByModerationChatIdAndModerationChatMessageId(666, 555)
+        verifyBlocking(voteDao) { delete(vote) }
+        verifyBlocking(api) { api.executeSuspended(answerCallbackQuery) }
+        verifyBlocking(api) { api.executeSuspended(editMessageReplyMarkup) }
+        verifyBlocking(api) { api.updateStatsInSenderChat(meme, listOf()) }
+        verifyNoMoreInteractions(memeDao, voteDao, metricsUtils, channelMarkupUpdater, api)
+    }
+
+    @Test
+    fun shouldUpdateVoteWhenTheSameVoterAndDifferentValue() {
+        val update = mock<VoteUpdate> {
+            on(it.isOld).thenReturn(false)
+            on(it.sourceChatId).thenReturn("666")
+            on(it.voterId).thenReturn(2103)
+            on(it.messageId).thenReturn(555)
+            on(it.callbackQueryId).thenReturn("random_callback")
+            on(it.voteValue).thenReturn(VoteValue.DOWN)
+            on(it.createdAt).thenReturn(1234567)
+        }
+
+        val meme = mock<Meme> {
+            whenever(it.channelMessageId).thenReturn(null)
+            whenever(it.moderationChatMessageId).thenReturn(321)
+            whenever(it.id).thenReturn(1)
+        }
+
+        val old = Vote(
+            1,
+            2103,
+            666,
+            VoteValue.UP,
+            Instant.ofEpochMilli(1234567)
+        )
+
+        whenever(memeDao.findMemeByModerationChatIdAndModerationChatMessageId(666, 555))
+            .thenReturn(meme to listOf(old))
+
+        val new = Vote(
+            1,
+            2103,
+            666,
+            VoteValue.DOWN,
+            Instant.ofEpochMilli(1234567)
+        )
+
+        val answerCallbackQuery = AnswerCallbackQuery().apply {
+            callbackQueryId = "random_callback"
+            text = "Вы засрали этот мем \uD83D\uDCA9"
+        }
+
+        val editMessageReplyMarkup = EditMessageReplyMarkup().apply {
+            chatId = telegramProperties.targetChatId
+            messageId = 321
+            replyMarkup = createMarkup(listOf(new))
+        }
+
+        api.stub {
+            onBlocking { executeSuspended(answerCallbackQuery) }.thenReturn(true)
+            onBlocking { executeSuspended(editMessageReplyMarkup) }.thenReturn(mock<Serializable>())
+        }
+
+        voteHandler.handleSync(update)
+
+        verify(memeDao).findMemeByModerationChatIdAndModerationChatMessageId(666, 555)
+        verifyBlocking(voteDao) { update(new) }
+        verifyBlocking(api) { api.executeSuspended(answerCallbackQuery) }
+        verifyBlocking(api) { api.executeSuspended(editMessageReplyMarkup) }
+        verifyBlocking(api) { api.updateStatsInSenderChat(meme, listOf(new)) }
+        verifyNoMoreInteractions(memeDao, voteDao, metricsUtils, channelMarkupUpdater, api)
     }
 
 }
